380f332aafef2c8d9f21a12835679c779952e92c
Artur Kedzior <artur@kedzior.es>
2025-05-28 19:26:44 +0200

    add one to one simple event support

---
+++ samples/Handlers/Abstractions/EventHandler.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+
+namespace Handlers.Abstractions;
+
+public abstract class EventHandler<TEvent> : MinimalCqrs.EventHandler<TEvent> where TEvent : IHandlerMessage
+{
+    protected readonly ILogger Logger;
+
+    protected EventHandler(IHandlerContext context)
+    {
+        Logger = context.Logger;
+    }
+}
---
+++ samples/Handlers/Orders/Events/OrderCreated.cs
@@ -0,0 +1,18 @@
+﻿namespace Handlers.Orders.Events;
+
+public static class OrderCreated
+{
+    public sealed record Event(string CustomerName) : IEvent;
+
+    public sealed class Handler : Abstractions.EventHandler<Event>
+    {
+        public Handler(IHandlerContext context) : base(context)
+        {
+        }
+
+        public override async Task ExecuteAsync(Event @event, CancellationToken ct = default)
+        {
+            await Task.FromResult($"OrderCreated for {@event.CustomerName}");
+        }
+    }
+}
